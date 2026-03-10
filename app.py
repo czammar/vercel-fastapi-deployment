cb1675f645e8e45985662a148706faa6c20342dd
czammar <cesar_czm@ciencias.unam.mx>
2026-03-10 08:19:14 -0600

    feat: adding root message

--- app.py
+++ app.py
@@ -84,6 +84,10 @@ app: FastAPI = FastAPI(
     version="1.0.0",
 )
 
+@app.get("/")
+def root():
+    return {"message": "Welcome to the root route!"}
+
 @app.get("/isAlive", response_model=HealthResponse)
 def is_alive() -> HealthResponse:
     """
